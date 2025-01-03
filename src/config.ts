5dd424d6dedf5dd891a3f901cfa87d6e22900295
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2025-01-03 14:56:16 +0100

    initialized config vars

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@
 // SCRAPE_CSS_SELECTOR: css selector to scrape data from a page
 // IGNORE_CSS_SELECTORS: array of css selectors to ignore when scraping
 
-export const ROOT: string =
+export const ROOT: string = ''
 
-export const SUBPAGES: string[] =
+export const SUBPAGES: string[] = []
 
-export const SCRAPE_CSS_SELECTOR: string =
-export const IGNORE_CSS_SELECTORS: string[] =
+export const SCRAPE_CSS_SELECTOR: string = ''
+export const IGNORE_CSS_SELECTORS: string[] = []
